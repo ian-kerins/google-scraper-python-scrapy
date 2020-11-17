d53a8acf955199a8d9a32b164ead4517e7e2d493
ian-kerins <60484445+ian-kerins@users.noreply.github.com>
2020-11-17 16:47:45 +0100

    Update google.py

--- google_scraper/spiders/google.py
+++ google_scraper/spiders/google.py
@@ -26,7 +26,7 @@ class GoogleSpider(scrapy.Spider):
     name = 'google'
     allowed_domains = ['api.scraperapi.com']
     custom_settings = {'ROBOTSTXT_OBEY': False, 'LOG_LEVEL': 'INFO',
-                       'CONCURRENT_REQUESTS_PER_DOMAIN': 10}
+                       'CONCURRENT_REQUESTS_PER_DOMAIN': 10, , 'RETRY_TIMES': 5}
 
     def start_requests(self):
         queries = ['scrapy', 'beautifulsoup'] ## Enter keywords here ['keyword1', 'keyword2', 'etc']
